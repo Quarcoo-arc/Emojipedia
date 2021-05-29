32d333c6fe57d430cde9a7f26b41765e201bee64
Quarcoo-arc <53879894+Quarcoo-arc@users.noreply.github.com>
2021-05-29 13:13:50 +0000

    Code Refactoring - Arrow Function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import emojipedia from "../emojipedia";
 import Emoji from "./Emoji";
 
-function createEmoji(emojipedia) {
-  return (
-    <Emoji
-      key={emojipedia.id}
-      emoji={emojipedia.emoji}
-      title={emojipedia.name}
-      content={emojipedia.meaning}
-    />
-  );
-}
+// function createEmoji(emojipedia) {
+//   return (
+//     <Emoji
+//       key={emojipedia.id}
+//       emoji={emojipedia.emoji}
+//       title={emojipedia.name}
+//       content={emojipedia.meaning}
+//     />
+//   );
+// }
 
 function App() {
   return (
@@ -19,7 +19,16 @@ function App() {
       <h1>
         <span>emojipedia</span>
       </h1>
-      <dl className="dictionary">{emojipedia.map(createEmoji)}</dl>
+      <dl className="dictionary">
+        {emojipedia.map((emojipedia) => (
+          <Emoji
+            key={emojipedia.id}
+            emoji={emojipedia.emoji}
+            title={emojipedia.name}
+            content={emojipedia.meaning}
+          />
+        ))}
+      </dl>
     </div>
   );
 }
